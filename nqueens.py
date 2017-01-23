0dcbaca015a71afcdfdbe051f84b8342ec52312c
xumx <xumx@tupo.com>
2017-01-23 10:29:54 +0800

    add new

--- nqueens.py
+++ nqueens.py
@@ -25,51 +25,62 @@ class Solution(object):
             r += 1
             c += 1
     
-    result = []
+    # result = set()
     def solveNQueens(self, n):
         """
         :type n: int
         :rtype: List[List[str]]
         """
-        # count = [[0] * n for _ in range(n)]
-        return self.solveNQueensByRow(n, [], 0)
+        self.result = []
+        self.solveNQueensByRow(n, [], 0, [], [])
+        # self.result.extend([n-i-1 for l in self.result for i in l if l[0] < n/2])
+        # return [[self.tostr(n, i)] for q in self.result for i in q]
+        return self.result
     
     def tostr(self, n, i):
         l = ['.'] * n
         l[i] = 'Q'
         return ''.join(l)
         
-    def solveNQueensByRow(self, n, queens, row):
+    def solveNQueensByRow(self, n, queens, row, diffset, sumset):
         if row >= n:
-            return buf
+            self.result.append([self.tostr(n, i) for i in queens])
+            # print queens
+            # self.result.append(queens)
+            return
             
+        # for j in range((n+1)/2):
         for j in range(n):
-            if count[row][j] > 0:
-                continue
+            # print j
+            # if count[row][j] > 0:
+                # continue
                 
-            self.mark(n, row, j, count, 1)
+            # self.mark(n, row, j, count, 1)
             
-            s = self.tostr(n, j)
+            # s = self.tostr(n, j)
+            if j not in queens and row-j not in diffset and row+j not in sumset:
+                # queens.append(j)
+                self.solveNQueensByRow(n, queens+[j], row+1, diffset+[row-j], sumset+[row+j])
             
-            if row + 1 == n:
-                buf.append([s])
-                # print buf, row
-            else:
-                subret = self.solveNQueensByRow(n, count, row+1)
-                if subret:
-                    # print subret, row, 'subret', buf, s
-                    for l in subret:
-                        buf.append([s] + l)
+            # if row + 1 == n:
+#                 buf.append([s])
+#                 # print buf, row
+#             else:
+#                 subret = self.solveNQueensByRow(n, count, row+1)
+#                 if subret:
+#                     # print subret, row, 'subret', buf, s
+#                     for l in subret:
+#                         buf.append([s] + l)
                     # print buf, row
                     
-            self.mark(n, row, j, count, -1)
-        return buf
+            # self.mark(n, row, j, count, -1)
+        # return buf
                 
 if __name__ == '__main__':
-    n = 5
-    count = [[0] * n for _ in range(n)]
+    # n = 5
+    # count = [[0] * n for _ in range(n)]
     s = Solution()
-    print s.solveNQueens(5)
+    print s.solveNQueens(4)
     # s.mark(n, 1, 1, count, 1)
 #     print count
 #     s.mark(n, 1, 1, count, -1)
